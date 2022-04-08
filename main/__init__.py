981f2ff8c2efc16eeee790c197b93cd36a5c440a
FaaaDev <faaadev@gmail.com>
2022-04-08 16:02:53 +0700

    upd

--- main/__init__.py
+++ main/__init__.py
@@ -379,8 +379,6 @@ def account(self):
         result = db.session.query(AccouMdb, KategMdb, KlasiMdb)\
             .join(AccouMdb, KategMdb.id == AccouMdb.kat_code)\
             .outerjoin(KlasiMdb, KategMdb.kode_klasi == KlasiMdb.id)\
-            .order_by(KategMdb.id.asc())\
-            .order_by(KlasiMdb.id.asc())\
             .order_by(AccouMdb.acc_code.asc()).all()
         print(result)
         data = [
