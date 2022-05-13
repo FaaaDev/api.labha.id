1ce4233c513915feb1531a140f6d5bc5e9e7ecf3
FaaaDev <faaadev@gmail.com>
2022-05-13 10:29:51 +0700

    minor

---
+++ main/model/preq_mdb.py
@@ -0,0 +1,19 @@
+from enum import unique
+from main.shared.shared import db
+
+class SubAreaMdb(db.Model):
+    __table_args__ = {'schema': 'master'}
+    __tablename__ = 'SUBAREAMDB'
+
+    id = db.Column(db.Integer, primary_key=True)
+    sub_code = db.Column(db.String(255), unique=True)
+    sub_area_code = db.Column(db.Integer)
+    sub_name = db.Column(db.String(255))
+    sub_ket = db.Column(db.Text)
+    
+
+    def __init__(self, sub_code, sub_area_code, sub_name, sub_ket):
+        self.sub_code = sub_code
+        self.sub_area_code = sub_area_code
+        self.sub_name = sub_name
+        self.sub_ket = sub_ket
