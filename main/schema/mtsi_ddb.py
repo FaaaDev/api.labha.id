48abb726004cf3230f0055cbdf56ec71015e7581
ProgrammerApri <apriyanaprasetioputra@gmail.com>
2022-08-17 17:44:29 +0700

    minor

--- main/schema/mtsi_ddb.py
+++ main/schema/mtsi_ddb.py
@@ -3,7 +3,7 @@ from main.shared.shared import ma
 class MtsiddbSchema(ma.Schema):
     class Meta:
         ordered=True
-        fields = ('id', 'mtsi_id', 'prod_id', 'qty')
+        fields = ('id', 'mtsi_id', 'prod_id', 'unit', 'qty')
 
 
 mtsiddb_schema = MtsiddbSchema()
