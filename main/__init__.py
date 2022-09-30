43d3ced6207df1c33a3bde21cb4670dbf70e61a3
FaaaDev <faaadev@gmail.com>
2022-09-30 14:25:57 +0700

    minor

--- main/__init__.py
+++ main/__init__.py
@@ -248,8 +248,7 @@ def token_required(f):
 
 @app.route("/")
 def index():
-    # return redirect(target_url())
-    return "Test"
+    return redirect(target_url())
 
 
 @app.route("/v1/api/login", methods=["POST"])
