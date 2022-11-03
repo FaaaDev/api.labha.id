9d75fee2edaadfdc145bf51ea792f6c50367f8c5
nrnidaaulia <nurnidaaulia99@gmail.com>
2022-11-03 10:48:19 +0700

    rev

--- main/function/update_ap_payment.py
+++ main/function/update_ap_payment.py
@@ -32,7 +32,7 @@ class UpdateApPayment():
             for x in acq:
                 if delete:
                     old_ap = ApCard.query.filter(
-                    and_(ApCard.acq_id == x.id, ApCard.pay_type == "H4")).first()
+                        and_(ApCard.acq_id == x.id, ApCard.pay_type == "H4")).first()
                     if old_ap:
                         db.session.delete(old_ap)
                         db.session.commit()
@@ -45,7 +45,8 @@ class UpdateApPayment():
                         .first()
                     )
 
-                    pembelian = ApCard.query.filter(and_(ApCard.ord_id == fk[1].id, ApCard.trx_type == "LP", ApCard.pay_type == "P1")).first()
+                    pembelian = ApCard.query.filter(and_(
+                        ApCard.ord_id == fk[1].id, ApCard.trx_type == "LP", ApCard.pay_type == "P1")).first()
                     old_ap = ApCard.query.filter(
                         and_(ApCard.acq_id == x.id, ApCard.pay_type == "H4")).first()
                     if old_ap:
@@ -53,14 +54,16 @@ class UpdateApPayment():
                         db.session.commit()
 
                     ap_card = ApCard(pembelian.sup_id, pembelian.ord_id, pembelian.ord_date, pembelian.ord_due, pembelian.po_id,
-                                    x.id, exp.exp_date, None, "k", pembelian.trx_type, "H4", pembelian.trx_amnh, None, x.payment, None, exp.giro_num, exp.giro_date)
+                                     x.id, exp.exp_date, None, "k", pembelian.trx_type, "H4", pembelian.trx_amnh, None, x.payment, None, exp.giro_num, exp.giro_date)
 
                     db.session.add(ap_card)
                     db.session.commit()
 
         if delete:
-            old_trans_sup = TransDdb.query.filter(and_(TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "D", TransDdb.trx_desc == "JURNAL PELUNASAN %s"%(exp.exp_code))).first()
-            old_trans_exp = TransDdb.query.filter(and_(TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "K", TransDdb.trx_desc == "JURNAL PELUNASAN %s"%(exp.exp_code))).first()
+            old_trans_sup = TransDdb.query.filter(and_(
+                TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "D", TransDdb.trx_desc == "JURNAL PELUNASAN %s" % (exp.exp_code))).first()
+            old_trans_exp = TransDdb.query.filter(and_(
+                TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "K", TransDdb.trx_desc == "JURNAL PELUNASAN %s" % (exp.exp_code))).first()
             if old_trans_sup:
                 db.session.delete(old_trans_sup)
             if old_trans_sup:
@@ -68,55 +71,56 @@ class UpdateApPayment():
 
         else:
             if exp.exp_type == 1:
-                old_trans_sup = TransDdb.query.filter(and_(TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "D", TransDdb.trx_desc == "JURNAL PELUNASAN %s"%(exp.exp_code))).first()
-                old_trans_exp = TransDdb.query.filter(and_(TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "K", TransDdb.trx_desc == "JURNAL PELUNASAN %s"%(exp.exp_code))).first()
+                old_trans_sup = TransDdb.query.filter(and_(
+                    TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "D", TransDdb.trx_desc == "JURNAL PELUNASAN %s" % (exp.exp_code))).first()
+                old_trans_exp = TransDdb.query.filter(and_(
+                    TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "K", TransDdb.trx_desc == "JURNAL PELUNASAN %s" % (exp.exp_code))).first()
                 if old_trans_sup:
                     db.session.delete(old_trans_sup)
                 if old_trans_sup:
                     db.session.delete(old_trans_exp)
-                
+
                 db.session.commit()
-                    
+
                 sup = (
-                        db.session.query(SupplierMdb, PajakMdb)
-                        .outerjoin(PajakMdb, PajakMdb.id == SupplierMdb.sup_ppn)
-                        .filter(SupplierMdb.id == exp.acq_sup)
-                        .first()
-                    )
+                    db.session.query(SupplierMdb, PajakMdb)
+                    .outerjoin(PajakMdb, PajakMdb.id == SupplierMdb.sup_ppn)
+                    .filter(SupplierMdb.id == exp.acq_sup)
+                    .first()
+                )
                 # insert jurnal ap
                 trans_sup = TransDdb(exp.exp_code, exp.exp_date, sup[0].sup_hutang, None, None,
-                                    None, None, None, None, total, "D", "JURNAL PELUNASAN %s"%(exp.exp_code), None, None)
+                                     None, None, None, None, total, "D", "JURNAL PELUNASAN %s" % (exp.exp_code), None, None)
 
                 trans_exp = TransDdb(exp.exp_code, exp.exp_date, exp.kas_acc if exp.acq_pay else exp.bank_acc, None, None,
-                                    None, None, None, None, total, "K", "JURNAL PELUNASAN %s"%(exp.exp_code), None, None)
+                                     None, None, None, None, total, "K", "JURNAL PELUNASAN %s" % (exp.exp_code), None, None)
 
                 db.session.add(trans_sup)
                 db.session.add(trans_exp)
                 db.session.commit()
             else:
-                old_trans_sup = TransDdb.query.filter(and_(TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "D", TransDdb.trx_desc == "JURNAL PENGELUARAN %s"%(exp.exp_code))).first()
-                old_trans_exp = TransDdb.query.filter(and_(TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "K", TransDdb.trx_desc == "JURNAL PENGELUARAN %s"%(exp.exp_code))).all()
+                old_trans_sup = TransDdb.query.filter(and_(
+                    TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "D", TransDdb.trx_desc == "JURNAL PENGELUARAN %s" % (exp.exp_code))).first()
+                old_trans_exp = TransDdb.query.filter(and_(
+                    TransDdb.trx_code == exp.exp_code, TransDdb.trx_dbcr == "K", TransDdb.trx_desc == "JURNAL PENGELUARAN %s" % (exp.exp_code))).all()
                 if old_trans_sup:
                     db.session.delete(old_trans_sup)
                 if old_trans_sup:
                     for x in old_trans_sup:
                         db.session.delete(x)
-                
+
                 db.session.commit()
 
-
-                total = 0;
+                total = 0
                 trans_exp = []
                 for x in exps:
                     total += x.value
                     trans_exp.append(TransDdb(exp.exp_code, exp.exp_date, x.acc_code, None, None,
-                                    None, None, None, None, x.value, "D", "JURNAL PENGELUARAN %s"%(exp.exp_code), None, None))
-                    
+                                              None, None, None, None, x.value, "D", "JURNAL PENGELUARAN %s" % (exp.exp_code), None, None))
+
                 # insert jurnal ap
                 trans_sup = TransDdb(exp.exp_code, exp.exp_date, exp.exp_acc, None, None,
-                                    None, None, None, None, total, "K", "JURNAL PENGELUARAN %s"%(exp.exp_code), None, None)
-
-                
+                                     None, None, None, None, total, "K", "JURNAL PENGELUARAN %s" % (exp.exp_code), None, None)
 
                 db.session.add(trans_sup)
                 db.session.add_all(trans_exp)
